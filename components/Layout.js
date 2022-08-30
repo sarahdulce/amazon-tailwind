cc8ef2ef0a478f9a3f291d8798b29bbaec1476cd
sarahdulce <sarahdulcemenezes@gmail.com>
2022-08-30 16:23:31 +0100

    atualizações

---
+++ components/Layout.js
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+import React from 'react';
+
+export default function Layout({ title, children }) {
+  return (
+    <>
+      <Head>
+        <title>{title ? title + ' - Amazon' : 'Amazon'}</title>
+        <meta name="description" content="Ecommerce Website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        <header>header</header>
+        <main>{children}</main>
+        <footer>footer</footer>
+      </div>
+    </>
+  );
+}

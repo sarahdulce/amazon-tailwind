7117a972b36ab5e121f7ed45addec9b243cf711a
sarahdulce <sarahdulcemenezes@gmail.com>
2022-09-01 19:51:13 +0100

    atualização detalhes de produtos

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -23,9 +23,35 @@ export default function ProductScreen() {
             src={product.image}
             alt={product.name}
             width={640}
-            heigth={640}
+            height={640}
             layout="responsive"
           ></Image>
+        </div>
+        <div>
+          <ul>
+            <li>
+              <h1 className="text-lg">{product.name}</h1>
+            </li>
+            <li>Category: {product.category}</li>
+            <li>Brand: {product.brand}</li>
+            <li>
+              {product.rating} if {product.numReviews} reviews
+            </li>
+            <li>Description: {product.description}</li>
+          </ul>
+        </div>
+        <div>
+          <div className="card p-5">
+            <div className="mb-2 flex justify-between">
+              <div>Price</div>
+              <div>${product.price}</div>
+            </div>
+            <div className="mb-2 flex justify-between">
+              <div>Status</div>
+              <div>{product.countInStock > 0 ? 'In stock' : 'Unavailable'}</div>
+            </div>
+            <button className="primary-button w-full">Add to cart</button>
+          </div>
         </div>
       </div>
     </Layout>

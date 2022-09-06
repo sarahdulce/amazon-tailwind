0c044fbb5c018e0296f0d28fb0b55f94e98e3445
sarahdulce <sarahdulcemenezes@gmail.com>
2022-09-06 21:43:20 +0100

    atualização carrinho de compras

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -16,7 +16,14 @@ export default function ProductScreen() {
   }
 
   const addToCartHandler = () => {
-    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity: 1 } });
+    const existItem = state.cart.cartItems.find((x) => x.slug === product.slug);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    if (product.countInStock < quantity) {
+      alert('Sorry. Product is out of stock');
+      return;
+    }
+
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
   };
 
   return (
